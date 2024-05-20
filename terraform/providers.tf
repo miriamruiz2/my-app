097e01d33a837795331440d379c0d65b00aa7f21
Miriam.r <miriamruiz142@gmail.com>
2024-05-20 20:55:47 +0200

    First commit before terraform init

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tf-bucket"
+    key = "backed"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
