6825ccd910d35f27b8b504496a15fcdc6b2648de
Miriam.r <miriamruiz142@gmail.com>
2024-05-21 19:46:05 +0200

    First commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_lb_target_group" "app-tg" {
     port = 80
     protocol = "HTTP"
     target_type = "ip"
-    vpc_id = module.vpc.vpd_id
+    vpc_id = module.vpc.vpc_id
     health_check {
       matcher = "200,301,302"
       path = "/"
