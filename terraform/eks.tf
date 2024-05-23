4782e33547a571ce6da3953b238a44daaffa0515
Miriam.r <miriamruiz142@gmail.com>
2024-05-23 22:51:01 +0200

    Second try

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "hello_world" {
   name     = var.cluster-name
   role_arn = "arn:aws:iam::741208424656:role/LabRole"
-  version  = "1.20"
+  version  = "1.27"
   
   vpc_config {
     security_group_ids = [aws_security_group.k8s_cluster.id]
